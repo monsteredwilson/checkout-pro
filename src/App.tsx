a4d4ec95c3de8e84bba194969ee378a8d492152e
Adryel César <adryelcesarm@gmail.com>
2023-05-07 21:00:24 -0300

    atualiza analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ToastContainer } from 'react-toastify'
 import './App.css'
 import { Router } from './routes'
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 		<>
 			<Router/>
 			<ToastContainer/>
+			<Analytics/>
 		</>
 	)
 }
